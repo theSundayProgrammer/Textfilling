068b7873ad88a4b2c3f2380883d8352618d450b1
Joe Mariadassou <theSundayProgrammer@gmail.com>
2020-03-17 10:25:53 +1100

    Adding memoisation

--- format_util.cpp
+++ format_util.cpp
@@ -1,10 +1,14 @@
 #include "format_util.h"
 #include <cassert>
+#include <map>
 namespace {
     struct context{
+			  typedef std::pair<int,int> key;
+				typedef std::pair<double,std::vector<int>> value;
         //Data
         const std::vector<int>& word_lengths;
         int                     line_length;
+				mutable std::map<key,value> cache;
         format_util::cost_function cost;
         //ctor
         context(const std::vector<int>& word_lengths_,
@@ -21,20 +25,29 @@ static double min_cost(
     int               b,
 		const context&    context
 ){
-    assert(result);
+    
+		assert(result);
     assert(0 <= a);
     assert(a <= b);
     double retval=0.0;
     int i=0;
 		int sum = 0;
+		auto val_ptr = context.cache.find({a,b});
+		if (val_ptr != context.cache.end()){
+			*result = val_ptr->second.second;
+			retval = val_ptr->second.first;
+			return retval;
+		}
 		for(i=a; i<=b;++i){
 			sum += context.word_lengths[i];
 			if (sum>context.line_length) break;
 		}
 		if(b == context.word_lengths.size() -1 && sum <= context.line_length){ // last line
 			retval = 0.0;
+			context.cache.insert({{a,b},{retval,std::vector<int>()}});
 		}else if(sum <= context.line_length)	{// a to b fits line
 			retval = context.cost(context.line_length - sum);
+			context.cache.insert({{a,b},{retval,std::vector<int>()}});
 		}else{ // need to break into several lines
 			std::vector<int> lowest_left;
 			std::vector<int> lowest_right;
@@ -58,6 +71,7 @@ static double min_cost(
 			result->push_back(lowest_k);
 			result->insert(result->end(),lowest_right.begin(),lowest_right.end());
 			retval=lowest_cost;
+			context.cache.insert({{a,b},{retval,*result}});
 		}
 		
     return retval;
