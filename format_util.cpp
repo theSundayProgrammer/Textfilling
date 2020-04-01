a309744c712e15c3ec3f662feb7d7f6edd955848
Joe Mariadassou <theSundayProgrammer@gmail.com>
2020-04-01 13:24:37 +1100

    Refactoring: Improve indendation

--- format_util.cpp
+++ format_util.cpp
@@ -23,58 +23,58 @@ static double min_cost(
     std::vector<int>* result,
     int               a,
     int               b,
-		const context&    context
-){
-	assert(result);
+    const context&    context
+    ){
+    assert(result);
     assert(0 <= a);
     assert(a < b);
     double retval=0.0;
     int i=0;
-		int sum = 0;
-		auto val_ptr = context.cache.find({a,b});
-		if (val_ptr != context.cache.end()){
-			std::tie(retval,*result) = val_ptr->second;
-			return retval;
-		}
-		for(i=a; i<b;++i){
-			sum += context.word_lengths[i];
-			if (sum>context.line_length) break;
-		}
-		if(b == context.word_lengths.size() && sum <= context.line_length){ // last line
-			retval = 0.0;
-			context.cache.insert({{a,b},{retval,std::vector<int>()}});
-		}else if(sum <= context.line_length)	{// a to b fits line
-			retval = context.cost(context.line_length - sum);
-			context.cache.insert({{a,b},{retval,std::vector<int>()}});
-		}else if (b-a ==1){ //if one word is longer than max string length
-			retval = 0;
-			context.cache.insert({{a,b},{retval,std::vector<int>()}});
-		}else{ // need to break into several lines
-			std::vector<int> lowest_left;
-			std::vector<int> lowest_right;
-			int lowest_k=0;
-			double lowest_cost=0.0;
-			const int first = a+1;
-			
-			for(int k = first; k<b; ++k){
-				std::vector<int> left;
-				std::vector<int> right;
-				double cost = min_cost(&left,a,k,context)+
-				              min_cost(&right, k,b,context);
-				if(k==first || cost<lowest_cost){
-					lowest_cost = cost;
-					lowest_k = k;
-					lowest_left.swap(left);
-					lowest_right.swap(right);
-				}
-			}
-			result->swap(lowest_left);
-			result->push_back(lowest_k);
-			result->insert(result->end(),lowest_right.begin(),lowest_right.end());
-			retval=lowest_cost;
-			context.cache.insert({{a,b},{retval,*result}});
-		}
-		
+    int sum = 0;
+    auto val_ptr = context.cache.find({a,b});
+    if (val_ptr != context.cache.end()){
+        std::tie(retval,*result) = val_ptr->second;
+        return retval;
+    }
+    for(i=a; i<b;++i){
+        sum += context.word_lengths[i];
+        if (sum>context.line_length) break;
+    }
+    if(b == context.word_lengths.size() && sum <= context.line_length){ // last line
+        retval = 0.0;
+        context.cache.insert({{a,b},{retval,std::vector<int>()}});
+    }else if(sum <= context.line_length)	{// a to b fits line
+        retval = context.cost(context.line_length - sum);
+        context.cache.insert({{a,b},{retval,std::vector<int>()}});
+    }else if (b-a ==1){ //if one word is longer than max string length
+        retval = 0;
+        context.cache.insert({{a,b},{retval,std::vector<int>()}});
+    }else{ // need to break into several lines
+        std::vector<int> lowest_left;
+        std::vector<int> lowest_right;
+        int lowest_k=0;
+        double lowest_cost=0.0;
+        const int first = a+1;
+
+        for(int k = first; k<b; ++k){
+            std::vector<int> left;
+            std::vector<int> right;
+            double cost = min_cost(&left,a,k,context)+
+                min_cost(&right, k,b,context);
+            if(k==first || cost<lowest_cost){
+                lowest_cost = cost;
+                lowest_k = k;
+                lowest_left.swap(left);
+                lowest_right.swap(right);
+            }
+        }
+        result->swap(lowest_left);
+        result->push_back(lowest_k);
+        result->insert(result->end(),lowest_right.begin(),lowest_right.end());
+        retval=lowest_cost;
+        context.cache.insert({{a,b},{retval,*result}});
+    }
+
     return retval;
 
 }
